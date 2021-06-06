cd303057cea83a68664987c7736e96945cbf312a
jcvroman <jcvroman@mac.com>
2021-06-06 17:41:21 -0600

    Initial Commit

---
+++ FastingReporter/FastingReporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastingReporterApp.swift
+//  FastingReporter
+//
+//  Created by Jimmy Vroman on 6/6/21.
+//
+
+import SwiftUI
+
+@main
+struct FastingReporterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
