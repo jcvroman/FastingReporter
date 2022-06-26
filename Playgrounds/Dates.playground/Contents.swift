b1d2b1afa2956b9bd04bcd2e8ff9dcda4c7d9114
jcvroman <jcvroman@mac.com>
2022-06-26 13:50:42 -0600

    Added Collections code to Dates playground
    FIXME: TODO: Collections playground not editable. Why? So, added Collections code to Dates for now.

--- Playgrounds/Dates.playground/Contents.swift
+++ Playgrounds/Dates.playground/Contents.swift
@@ -27,3 +27,26 @@ let diffMinutes = Calendar.current
 let diffHours = Calendar.current
     .dateComponents([.hour], from: now, to: soon)
     .hour
+
+
+// FIXME: TODO: Collections playground not editable. Why? So, added Collections code to Dates for now.
+// import Darwin
+// import UIKit
+
+// Collections.
+// Compare adjacent elements in a collection.
+let array = [9, 1, 3, 5, 10, 2, 4, 7, 1]
+var biggest = 0
+
+for (lhs, rhs) in zip(array, array.dropFirst()) {
+    if lhs > rhs {
+        print("\(lhs) > \(rhs)")
+        print("do something")
+        if lhs > biggest { biggest = lhs }
+    } else {
+        print("\(lhs) <= \(rhs)")
+        print("do nothing")
+    }
+}
+
+print("biggest: \(biggest)")
