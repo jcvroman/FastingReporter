a1c70a379f1430131851435b4c0a35f0c47e3cc5
jcvroman <jcvroman@mac.com>
2022-06-27 15:41:43 -0600

    Update item value in current from adjacent element in a collection
    Update item value in current element from adjacent element in a collection.

--- Playgrounds/Dates.playground/Contents.swift
+++ Playgrounds/Dates.playground/Contents.swift
@@ -1,6 +1,6 @@
-import Darwin
+// import Darwin
 import UIKit
-
+/*
 // Dates.
 // Calcuate time between dates (e.g. seconds, minutes).
 let now = Date.now
@@ -27,12 +27,14 @@ let diffMinutes = Calendar.current
 let diffHours = Calendar.current
     .dateComponents([.hour], from: now, to: soon)
     .hour
+*/
 
 
 // FIXME: TODO: Collections playground not editable. Why? So, added Collections code to Dates for now.
 // import Darwin
 // import UIKit
 
+/*
 // Collections.
 // Compare adjacent elements in a collection.
 let array = [9, 1, 3, 5, 10, 2, 4, 7, 1]
@@ -50,3 +52,34 @@ for (lhs, rhs) in zip(array, array.dropFirst()) {
 }
 
 print("biggest: \(biggest)")
+*/
+
+// Update item value in current element from adjacent element in a collection.
+struct CarbModel {
+    var id: Int
+    var carbs: Int
+    var nextCarbs: Int
+}
+
+var carbsList: [CarbModel] = []
+var carbsList2: [CarbModel] = []
+var carbs1: CarbModel = CarbModel(id: 1, carbs: 10, nextCarbs: 0)
+var carbs2: CarbModel = CarbModel(id: 2, carbs: 20, nextCarbs: 0)
+carbsList.append(carbs1)
+carbsList.append(carbs2)
+
+
+for (var lhs, rhs) in zip(carbsList, carbsList.dropFirst()) {
+    print("lhs.id: \(lhs.id) | lhs.carbs: \(lhs.carbs) | lhs.nextCarbs: \(lhs.nextCarbs)")
+    print("rhs.id: \(rhs.id) | rhs.carbs: \(rhs.carbs) | rhs.nextCarbs: \(rhs.nextCarbs)")
+    lhs.nextCarbs = rhs.carbs
+    carbsList2.append(lhs)
+    print("lhs.id: \(lhs.id) | lhs.carbs: \(lhs.carbs) | lhs.nextCarbs: \(lhs.nextCarbs)")
+    print("rhs.id: \(rhs.id) | rhs.carbs: \(rhs.carbs) | rhs.nextCarbs: \(rhs.nextCarbs)")
+}
+
+for element in carbsList2 {
+    print("element.id: \(element.id) | element.carbs: \(element.carbs) | element.nextCarbs: \(element.nextCarbs)")
+}
+
+
