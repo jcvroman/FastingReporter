de2e30bf420d45c54052782215156462bcbf5b64
jcvroman <jcvroman@mac.com>
2022-07-14 15:34:26 -0600

    ERRORS: FIXED: SwiftLint Errors
    ERROR: Force Cast Violation: Force casts should be avoided.
    FIXED: Fixed #4 for original commit for feature #4 .

--- FastingReporter/Helpers/Constants.swift
+++ FastingReporter/Helpers/Constants.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public enum Constants {
-    static let appName: String = Bundle.main.infoDictionary?["CFBundleName"] as! String
-    static let appVersion: String = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as! String
-    static let buildVersion: String = Bundle.main.infoDictionary?["CFBundleVersion"] as! String
+    static let appName: String = Bundle.main.infoDictionary?["CFBundleName"] as? String ?? "appName"
+    static let appVersion: String = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "appVersion"
+    static let buildVersion: String = Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "buildVersion"
 }
