2590c88848352541d4b22a8e0766dde1adeda0eb
jcvroman <jcvroman@mac.com>
2022-07-18 22:24:20 -0600

    Updated Fast Color Level values
    Updated Fast Color Level values.

--- FastingReporter/Helpers/Constants.swift
+++ FastingReporter/Helpers/Constants.swift
@@ -21,8 +21,8 @@ public enum Constants {
 
     // MARK: - Ints: Miscellaneous.
     static let fastColorLevelA: Int = 16
-    static let fastColorLevelB: Int = 15
-    static let fastColorLevelC: Int = 14
+    static let fastColorLevelB: Int = 14
+    static let fastColorLevelC: Int = 12
 
     // MARK: - Colors: Miscellaneous.
     static let fastColorLevelAColor: Color = .green
